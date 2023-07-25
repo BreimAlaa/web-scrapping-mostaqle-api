d1b76f2c23d8665452f9cd23a26176f81346cc38
BreimAlaa <breim.alaa@gmail.com>
2023-07-25 10:18:41 +0300

    Added: exacting jobs from the website web

--- Extractor.py
+++ Extractor.py
@@ -28,9 +28,34 @@ class Extractor:
         skills = self.driver.find_element(By.TAG_NAME,"pre").text
         return skills
 
+    def get_parms(self, list:str, key:str, first:bool = False):
+        if len(list) == 0:
+            return ""
+        if first:
+            st = "?"
+        else:
+            st = "&"
+        return f"{st}{key}=" + list
 
-    # https: // mostaql.com / projects?category = business, development & budget_min = 50 & budget_max = 10000
-    def get_projects(self, categories: list = [], skills:list = [], budget_min: int = 0, budget_max: int = 1000000):
-        self.driver.get("https://mostaql.com/projects?")
-        projects = self.driver.find_elements(By.CLASS_NAME, "project-card")
-        return projects
+    def get_projects(self, page = 1, categories: list = [], skills:list = [], budget_min: int = 0, budget_max: int = 1000000):
+
+        categories_params = self.get_parms(categories, "category")
+        skills_params = self.get_parms(skills, "skills")
+
+        self.driver.get(f"https://mostaql.com/projects?page={page}{categories_params}{skills_params}&budget_min={budget_min}&budget_max={budget_max}")
+
+        job_wrappers = self.driver.find_elements(By.CLASS_NAME,'project-row')
+
+        values = {}
+
+        for job_wrapper in job_wrappers:
+            job_title_wrapper = job_wrapper.find_element(By.CLASS_NAME, 'mrg--bt-reset')
+            job_title = job_title_wrapper.find_element(By.TAG_NAME, 'a').text
+            job_link = job_title_wrapper.find_element(By.TAG_NAME, 'a').get_attribute('href')
+
+            job_decription = job_wrapper.find_element(By.CLASS_NAME, 'details-url').text
+            id = job_link.split('project')[1].split('-')[0].replace('/', '');
+            link = f'https://mostaql.com/project/{id}'
+            values[id] = {'job_title':job_title, 'job_descrption':job_decription, 'job_link':link}
+
+        return values
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi import FastAPI, BackgroundTasks, HTTPException, Query
+
 from Extractor import Extractor
 from Driver import Driver
 from fastapi import Response
+from typing import Annotated
 
 app = FastAPI()
 driver = Driver()
@@ -20,13 +22,16 @@ async def demo_get(email: str, password: str):
 
 # get skills
 @app.get("/skills")
-async def get_skills(page:int = 1, query:str = ""):
+async def get_skills(page:int = 1, query:str = ''):
     skills = extractor.get_skills(page, query)
     return Response(content=skills, media_type="application/json")
 
-# get projects
 @app.get("/projects")
-async def get_projects(categories: list = [], skills:list = [], budget_min: int = 0, budget_max: int = 1000000):
-    extract_projects(driver, categories)
-    projects = getProjects(driver)
+async def get_projects(page:int = 1, categories: str = '', skills: str = '', budget_min: int = 0, budget_max: int = 1000000):
+    projects = extractor.get_projects(page, categories, skills, budget_min, budget_max)
     return {"projects": projects}
+
+@app.get("/projects/{id}")
+async def get_project(id: str):
+    project = extractor.get_project(id)
+    return {"project": project}
